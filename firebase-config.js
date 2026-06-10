cfff5ee686b13a6048a63d4f4c325f7e327b9985
kantaro10484 <fujita_kantaro@michinoeki-toyohashi.jp>
2026-06-10 15:51:42 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Firebase 設定
+var firebaseConfig = {
+  apiKey:            "AIzaSyDb-uwn9zSnFb0aKgAMtBTRaY5bKtObgR8",
+  authDomain:        "backyard-park.firebaseapp.com",
+  projectId:         "backyard-park",
+  storageBucket:     "backyard-park.firebasestorage.app",
+  messagingSenderId: "971199821777",
+  appId:             "1:971199821777:web:ac95b7c72983be46b592f0"
+};
